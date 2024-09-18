4e6a5c1e23248ca531bf930240db90956245819a
Alona Shelikhova <ashelyaa17@gmail.com>
2024-09-18 13:45:00 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,23 @@
+"use strict";
+const pushNotification = (posTop, posRight, title, description, type)=>{
+    const div = document.createElement("div");
+    const h2 = document.createElement("h2");
+    const p = document.createElement("p");
+    div.classList.add("notification", type);
+    h2.classList.add("title");
+    h2.textContent = title;
+    p.textContent = description;
+    div.style.top = `${posTop}px`;
+    div.style.right = `${posRight}px`;
+    h2.style.whiteSpace = "nowrap";
+    div.append(h2, p);
+    document.body.append(div);
+    setTimeout(()=>{
+        div.style.display = "none";
+    }, 2000);
+};
+pushNotification(10, 10, "Title of Success message", "Message example.\n Notification should contain title and description.", "success");
+pushNotification(150, 10, "Title of Error message", "Message example.\n Notification should contain title and description.", "error");
+pushNotification(290, 10, "Title of Warning message", "Message example.\n Notification should contain title and description.", "warning");
+
+//# sourceMappingURL=index.f75de5e1.js.map
